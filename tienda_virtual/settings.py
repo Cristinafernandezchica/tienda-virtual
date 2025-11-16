7820108d1db21dbe4daba83ae0aeeee379aef182
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2025-11-17 00:44:01 +0100

    🔧 chore(settings): update allowed hosts for render deployment
    - update allowed hosts to include tractor-amarillo.onrender.com

--- tienda_virtual/settings.py
+++ tienda_virtual/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-default")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['cristinafdezchica.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['tractor-amarillo.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
